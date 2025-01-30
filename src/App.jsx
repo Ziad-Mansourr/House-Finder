a344d046db286766576ee6abcab3ae0526eb7a85
Ziad-Mansourr <01556746404ziad@gmail.com>
2025-01-30 04:38:22 +0200

    Forget Password

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import toast, { Toaster } from 'react-hot-toast';
 import "flowbite";
 import SignUp from './components/SignUp/SignUp'
-// import ForgetPassword from './components/ForgetPassword/ForgetPassword'
+import ForgetPassword from './components/ForgetPassword/ForgetPassword'
 import Layout from './components/Layout/Layout'
 function App() {
 
@@ -15,7 +15,7 @@ function App() {
   let router = createHashRouter([
     {
       path: '', element: <Layout /> , children: [
-      // {path: 'forgetPassword', element: <ForgetPassword />},
+      {path: 'forgetPassword', element: <ForgetPassword />},
       {path: 'signUp', element: <SignUp />}
 
     ]}
