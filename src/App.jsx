4561c3e872334a01f606231b8793decfb578103b
Ziad-Mansourr <01556746404ziad@gmail.com>
2025-01-30 04:35:29 +0200

    sign up

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,28 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import toast, { Toaster } from 'react-hot-toast';
 import "flowbite";
+import SignUp from './components/SignUp/SignUp'
+// import ForgetPassword from './components/ForgetPassword/ForgetPassword'
+import Layout from './components/Layout/Layout'
 function App() {
 
   const queryClient = new QueryClient()
    
   let router = createHashRouter([
     {
-    }
+      path: '', element: <Layout /> , children: [
+      // {path: 'forgetPassword', element: <ForgetPassword />},
+      {path: 'signUp', element: <SignUp />}
+
+    ]}
+    
   ])
+
+  // forgetPassword#/forgetPassword
+  // signUp#/signUp
   return (
     <>
- 
+        <RouterProvider router={router}></RouterProvider>
     </>
   )
 }
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react'
+import { Outlet } from 'react-router-dom'
+
+export default function Layout() {
+  return (
+    <>
+      <Outlet></Outlet>
+
+    </>
+  )
+}
