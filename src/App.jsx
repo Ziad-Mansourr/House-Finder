40343ed0ae120e2c5ab7172658082a02922ff7a4
fatmaahmed0 <fatmaahmed3255@gmail.com>
2025-02-04 18:50:47 +0200

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,17 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import toast, { Toaster } from 'react-hot-toast';
 import "flowbite";
+import Login from './components/Login/Login'
 function App() {
 
   const queryClient = new QueryClient()
    
   let router = createHashRouter([
-    {
-    }
+      {index:true , element: <Login/>}
   ])
   return (
     <>
- 
+      <RouterProvider router={router}></RouterProvider>
     </>
   )
 }
